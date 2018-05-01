bb85b79ae5fe4c37771babb88eb32199ad30bd57
Pavel <visualyg@gmail.com>
2018-05-01 16:08:05 +0300

    S
    1

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+    public static DialogueManager Instance { set; get; }
+
+    public Text nameText;
+    public Text dialogueText;
+
+    //moe
+    private bool dialogueIsStarted = false;
+
+    private Queue <string> sentences;
+    private Client client;
+
+	void Start () {
+        Instance = this;
+        sentences = new Queue<string>();
+        client = FindObjectOfType<Client>();
+
+	}
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        if (dialogueIsStarted == false)
+        {
+
+            dialogueIsStarted = true;
+
+            Debug.Log("Send Start Dialogue");
+            string msg = "CKLI|";
+            msg += "Start";
+            client.Send(msg);
+
+            nameText.text = dialogue.name;
+
+            sentences.Clear();
+
+            foreach (string sentence in dialogue.sentences)
+            {
+                sentences.Enqueue(sentence);
+            }
+
+            DisplayNextSentence();
+        }
+        
+
+
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        if(sentences.Count != 3)
+        {
+            Debug.Log("Send DisplayNextSentence to clients on Continue button click");
+            string msg = "CKLI|";
+            msg += "Continue";
+            client.Send(msg);
+
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+	
+    void EndDialogue()
+    {
+        Debug.Log("End of conversation. ");
+        dialogueIsStarted = false;
+    }
+
+}
---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour {
+
+    //moe
+    public static DialogueTrigger Instance { set; get; }
+
+    public Dialogue dialogue;
+
+    //moe 
+    private void Start()
+    {
+        Instance = this;
+    }
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+}
